1c3e2a98763dcf7181c67f3a339fe66127f61f7a
Brian Cardarella <bcardarella@gmail.com>
2013-01-02 12:55:43 -0500

    Updated ember dep and fixes for production

--- config/initializers/handlebars_assets.rb
+++ config/initializers/handlebars_assets.rb
@@ -1,2 +1,4 @@
-HandlebarsAssets::Config.template_namespace = 'Ember.TEMPLATES'
-HandlebarsAssets::Config.ember = true
+if defined?(HandlebarsAssets)
+  HandlebarsAssets::Config.template_namespace = 'Ember.TEMPLATES'
+  HandlebarsAssets::Config.ember = true
+end
